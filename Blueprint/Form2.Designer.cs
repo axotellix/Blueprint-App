dc7797bff0e932262cb013c4dc6ff66ca357fb35
axotellix <RSAndrey@list.ru>
2026-02-08 00:40:48 +0400

    created > Form2 (to test DB ORM)

---
+++ Blueprint/Form2.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Blueprint {
+    partial class Form2 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            winClose1 = new Blueprint.Components.WinControls.WinClose();
+            winMinimize1 = new Blueprint.Components.WinControls.WinMinimize();
+            mainContainer1 = new Blueprint.Components.MainContainer();
+            SuspendLayout();
+            // 
+            // winClose1
+            // 
+            winClose1.BackColor = Color.FromArgb(19, 20, 21);
+            winClose1.BorderWidth = 0;
+            winClose1.Location = new Point(12, 12);
+            winClose1.Name = "winClose1";
+            winClose1.Size = new Size(30, 30);
+            winClose1.TabIndex = 0;
+            // 
+            // winMinimize1
+            // 
+            winMinimize1.BackColor = Color.FromArgb(19, 20, 21);
+            winMinimize1.BorderWidth = 0;
+            winMinimize1.Location = new Point(48, 12);
+            winMinimize1.Name = "winMinimize1";
+            winMinimize1.Size = new Size(30, 30);
+            winMinimize1.TabIndex = 1;
+            // 
+            // mainContainer1
+            // 
+            mainContainer1.BackColor = Color.FromArgb(19, 20, 21);
+            mainContainer1.Location = new Point(31, 70);
+            mainContainer1.Name = "mainContainer1";
+            mainContainer1.Size = new Size(1160, 593);
+            mainContainer1.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1222, 698);
+            Controls.Add(mainContainer1);
+            Controls.Add(winMinimize1);
+            Controls.Add(winClose1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Components.WinControls.WinClose winClose1;
+        private Components.WinControls.WinMinimize winMinimize1;
+        private Components.MainContainer mainContainer1;
+    }
+}
---
+++ Blueprint/Form2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Blueprint.Components;
+using Blueprint.Components.Tabs;
+using Blueprint.Utils.UI;
+
+namespace Blueprint {
+    public partial class Form2 : GlassUIForm {
+        public Form2()
+        {
+            // set up > Form style
+            this.BackColor = GlassUIColors.Dark;
+            this.FormOpacity = 220;
+            this.EFFECT = ACCENT_ENABLE_BLURBEHIND;
+
+            InitializeComponent();
+        }
+    }
+}
